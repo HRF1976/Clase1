3a44aacb5d5395d18bd5addd0111d7c7923f620b
HRF1976 <hernan.formento@gmail.com>
2021-05-28 18:17:09 -0300

    calculadora

---
+++ Javascript/app.js
@@ -0,0 +1,20 @@
+
+// Defino las variables
+
+var numero1;
+var numero2;
+
+let suma;
+let resta;
+let division;
+let multiplicacion;
+
+const PI = 3.1416;
+
+// 2.es un método o proceso que se repite
+function inicio(){
+    numero1=prompt('Introduce el número 1: ')
+    numero2=prompt('Introduce otro número: ')
+    console.log(numero1);
+    // console.log('El segundo número es ', ${numero2})
+}
