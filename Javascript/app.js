fc34598c2cb94ca2237230d869a986553072b3d6
HRF1976 <hernan.formento@gmail.com>
2021-05-28 19:15:26 -0300

    suma y resta casi ok

--- Javascript/app.js
+++ Javascript/app.js
@@ -4,10 +4,10 @@
 var numero1;
 var numero2;
 
-let suma;
-let resta;
-let division;
-let multiplicacion;
+let suma=0;
+let resta=0;
+let division=0;
+let multiplicacion=0;
 
 const PI = 3.1416;
 
@@ -16,5 +16,34 @@ function inicio(){
     numero1=prompt('Introduce el número 1: ')
     numero2=prompt('Introduce otro número: ')
     console.log(numero1);
-    // console.log('El segundo número es ', ${numero2})
+    console.log('El segundo número es: ${numero2}')
 }
+
+numero1=parseFloat(numero1)
+numero2=parseFloat(numero2)
+
+function sumar(){
+    if (numero1 == undefined && numero2 == undefined) {
+        alert("Cargar números por favor")
+    } else {
+        suma=numero1+numero2
+        console.log(suma)
+    }
+};
+
+function restar() {
+    if (numero1 == undefined && numero2 == undefined) {
+        alert("Cargar números por favor")
+    } else {
+        suma=numero1-numero2
+        console.log(suma)
+    }
+};
+
+function multiplicar(){
+
+};
+
+function dividir (){
+
+};
